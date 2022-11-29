9659bcb8ce3c74aa166640f521d7d1d3566e0874
Michael Spencer <ms53150@gmail.com>
2022-11-28 20:29:55 -0500

    Add expense item component

--- section_03-react-basics-components/src/App.js
+++ section_03-react-basics-components/src/App.js
@@ -1,9 +1,15 @@
+// import logo from "./logo.svg";
+import ExpenseItem from "./components/ExpenseItem";
+import "./App.css";
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+	return (
+		<div>
+			<h2>Let's get Started!</h2>
+			{/* Adding a component */}
+			<ExpenseItem></ExpenseItem>
+		</div>
+	);
 }
 
 export default App;
---
+++ section_03-react-basics-components/src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+// components in react are functions
+// components can only have one element
+function ExpenseItem() {
+	return (
+		<div>
+			<div>Date</div>
+			<div>
+				<h2>Car Insurance</h2>
+				<div>$294.67</div>
+			</div>
+		</div>
+	);
+}
+
+export default ExpenseItem;
