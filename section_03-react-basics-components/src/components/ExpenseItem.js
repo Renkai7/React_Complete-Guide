92db3b318c9081c805a811df51cab989c9f355fc
Michael Spencer <ms53150@gmail.com>
2022-11-29 20:38:51 -0500

    Add props to items

--- section_03-react-basics-components/src/components/ExpenseItem.js
+++ section_03-react-basics-components/src/components/ExpenseItem.js
@@ -1,15 +1,14 @@
 // import styling
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-	const expenseDate = new Date(2021, 2, 28);
-
+// adding props in parameter
+function ExpenseItem(props) {
 	return (
 		<div className="expense-item">
-			<div>March 28th 2021</div>
+			<div>{props.date.toISOString()}</div>
 			<div className="expense-item__description">
-				<h2>Car Insurance</h2>
-				<div className="expense-item__price">$294.67</div>
+				<h2>{props.title}</h2>
+				<div className="expense-item__price">${props.amount}</div>
 			</div>
 		</div>
 	);
@@ -20,3 +19,4 @@ export default ExpenseItem;
 // Notes
 // components can only have one element
 // components in react are functions
+// use props by adding parameter
