a45510de07beb0d3ee6bc48716bd51eb1f8f3ced
Michael Spencer <ms53150@gmail.com>
2022-12-02 22:55:19 -0500

    change div to card component

--- section_03-react-basics-components/src/components/ExpenseItem.js
+++ section_03-react-basics-components/src/components/ExpenseItem.js
@@ -3,17 +3,18 @@ import "./ExpenseItem.css";
 
 // import component
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 // adding props in parameter
 function ExpenseItem(props) {
 	return (
-		<div className="expense-item">
+		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
 				<h2>{props.title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
 			</div>
-		</div>
+		</Card>
 	);
 }
 
