c9340dbf31982a8370588516b052021bcb0213fa
Michael Spencer <ms53150@gmail.com>
2022-12-02 22:53:47 -0500

    Create card component

---
+++ section_03-react-basics-components/src/components/Card.js
@@ -0,0 +1,20 @@
+import "./Card.css";
+
+function Card(props) {
+	// allows use of class in ExpenseItem.js (or component that Card will be used in)
+	const classes = "card " + props.className;
+	return <div className={classes}>{props.children}</div>;
+}
+
+export default Card;
+
+// props.children allows you to place content into component (shown below):
+// {
+// 	/* <Card className="expense-item">
+// 	<ExpenseDate date={props.date} />
+// 	<div className="expense-item__description">
+// 		<h2>{props.title}</h2>
+// 		<div className="expense-item__price">${props.amount}</div>
+// 	</div>
+// </Card>; */
+// }
