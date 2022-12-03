21ab3fb633624cd8f4ef2a2e5cd8b7d4b330ce80
Michael Spencer <ms53150@gmail.com>
2022-12-03 18:15:10 -0500

    create folders for UI and Expenses

--- section_03-react-basics-components/src/components/Expenses/ExpenseItem.js
+++ section_03-react-basics-components/src/components/Expenses/ExpenseItem.js
@@ -3,7 +3,7 @@ import "./ExpenseItem.css";
 
 // import component
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 // adding props in parameter
 function ExpenseItem(props) {
