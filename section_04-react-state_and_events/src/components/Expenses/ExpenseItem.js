670343129a695f77a8c54ab728704ec40178ff5d
Michael Spencer <ms53150@gmail.com>
2022-12-05 21:25:11 -0500

    add useState hook for button

--- section_04-react-state_and_events/src/components/Expenses/ExpenseItem.js
+++ section_04-react-state_and_events/src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,18 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+	// Hook - useState
+	// Array Destructuring = const [title, setTitle]
+	const [title, setTitle] = useState(props.title);
+
 	const clickHandler = () => {
+		// Call setTitle to change element
+		setTitle("Updated!");
 		console.log("Clicked!");
 	};
 
@@ -12,7 +20,7 @@ const ExpenseItem = (props) => {
 		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
 				<button onClick={clickHandler}>Change Title</button>
 			</div>
@@ -22,4 +30,8 @@ const ExpenseItem = (props) => {
 
 export default ExpenseItem;
 
+// Notes:
 // Give button a function using 'onClick' property
+// '{}' when importing useState is for importing names
+// useState is a React hook (hooks start with 'use')
+// useState takes in the initial value (props.title)
