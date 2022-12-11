bc172e6cc5d8498b31b0e43f9cb2eeb0ba1d017d
Michael Spencer <ms53150@gmail.com>
2022-12-10 20:18:22 -0500

    create function to store data for new expense

--- section_04-react-state_and_events/src/components/New Expense/NewExpense.js
+++ section_04-react-state_and_events/src/components/New Expense/NewExpense.js
@@ -4,10 +4,22 @@ import ExpenseForm from "./ExpenseForm";
 
 import "./NewExpense.css";
 
-const NewExpense = () => {
+// Sets up new expenses to be added
+const NewExpense = (props) => {
+	const saveExpenseDataHandler = (enteredExpenseData) => {
+		// object that holds data from submitHandler in ExpenseForm
+		const expenseData = {
+			...enteredExpenseData,
+			id: Math.random().toString(),
+		};
+		// console.log(expenseData);
+		// from App.js NewExpense prop
+		props.onAddExpense(expenseData);
+	};
 	return (
 		<div className="new-expense">
-			<ExpenseForm />
+			{/* name of prop can be anything */}
+			<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
 		</div>
 	);
 };
