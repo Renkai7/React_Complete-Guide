464e5b11b8f768f63d48a5d7d133b62c34a731ef
Michael Spencer <ms53150@gmail.com>
2022-12-11 22:06:25 -0500

    add check for expense content for year

--- section_05-react-render-lists_conditional-content/src/components/Expenses/Expenses.js
+++ section_05-react-render-lists_conditional-content/src/components/Expenses/Expenses.js
@@ -20,6 +20,21 @@ const Expenses = (props) => {
 		return expense.date.getFullYear().toString() === filteredYear;
 	});
 
+	// Default value for content
+	let expensesContent = <p>No expenses found.</p>;
+	// checks for an expense then displays components for expense items
+	if (filteredExpenses.length > 0) {
+		expensesContent = filteredExpenses.map((expense) => (
+			/* Creates an array of ExpenseItem components, allowing you to add more ExpenseItems*/
+			<ExpenseItem
+				key={expense.id}
+				title={expense.title}
+				amount={expense.amount}
+				date={expense.date}
+			/>
+		));
+	}
+
 	return (
 		<div>
 			<Card className="expenses">
@@ -27,15 +42,8 @@ const Expenses = (props) => {
 					selected={filteredYear}
 					onChangeFilter={filterChangeHandler}
 				/>
-				{/* Creates an array of ExpenseItem components, allowing you to add more ExpenseItems*/}
-				{filteredExpenses.map((expense) => (
-					<ExpenseItem
-						key={expense.id}
-						title={expense.title}
-						amount={expense.amount}
-						date={expense.date}
-					/>
-				))}
+				{/* Expense Item or 'No expenses found' */}
+				{expensesContent}
 			</Card>
 		</div>
 	);
