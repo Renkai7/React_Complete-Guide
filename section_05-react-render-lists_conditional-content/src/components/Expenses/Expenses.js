d743e8c6568ccc56f36e3d9422b78944cb911160
Michael Spencer <ms53150@gmail.com>
2022-12-12 17:14:16 -0500

    create component for list of expenses

--- section_05-react-render-lists_conditional-content/src/components/Expenses/Expenses.js
+++ section_05-react-render-lists_conditional-content/src/components/Expenses/Expenses.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 
 import "./Expenses.css";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
 	// Default value for year in 'Filter by year' dropdown
@@ -20,21 +20,6 @@ const Expenses = (props) => {
 		return expense.date.getFullYear().toString() === filteredYear;
 	});
 
-	// Default value for content
-	let expensesContent = <p>No expenses found.</p>;
-	// checks for an expense then displays components for expense items
-	if (filteredExpenses.length > 0) {
-		expensesContent = filteredExpenses.map((expense) => (
-			/* Creates an array of ExpenseItem components, allowing you to add more ExpenseItems*/
-			<ExpenseItem
-				key={expense.id}
-				title={expense.title}
-				amount={expense.amount}
-				date={expense.date}
-			/>
-		));
-	}
-
 	return (
 		<div>
 			<Card className="expenses">
@@ -43,7 +28,7 @@ const Expenses = (props) => {
 					onChangeFilter={filterChangeHandler}
 				/>
 				{/* Expense Item or 'No expenses found' */}
-				{expensesContent}
+				<ExpensesList items={filteredExpenses} />
 			</Card>
 		</div>
 	);
