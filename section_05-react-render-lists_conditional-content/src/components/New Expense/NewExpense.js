095e8394b9fb01e9f8ac997e0559b11095cab29e
Michael Spencer <ms53150@gmail.com>
2022-12-13 19:58:52 -0500

    add hook for edit states

--- section_05-react-render-lists_conditional-content/src/components/New Expense/NewExpense.js
+++ section_05-react-render-lists_conditional-content/src/components/New Expense/NewExpense.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 
@@ -6,20 +6,40 @@ import "./NewExpense.css";
 
 // Sets up new expenses to be added
 const NewExpense = (props) => {
+	const [isEditing, setIsEditing] = useState(false);
 	const saveExpenseDataHandler = (enteredExpenseData) => {
 		// object that holds data from submitHandler in ExpenseForm
 		const expenseData = {
 			...enteredExpenseData,
 			id: Math.random().toString(),
 		};
-		// console.log(expenseData);
 		// from App.js NewExpense prop
 		props.onAddExpense(expenseData);
+		setIsEditing(false);
 	};
+
+	// changes state to 'Editing' state
+	// state used to switch between 'add expense button' and 'expense form'
+	const startEditingHandler = () => {
+		setIsEditing(true);
+	};
+
+	const stopEditingHandler = () => {
+		setIsEditing(false);
+	};
+
 	return (
 		<div className="new-expense">
+			{!isEditing && (
+				<button onClick={startEditingHandler}>Add New Expense</button>
+			)}
 			{/* name of prop can be anything */}
-			<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+			{isEditing && (
+				<ExpenseForm
+					onSaveExpenseData={saveExpenseDataHandler}
+					onCancel={stopEditingHandler}
+				/>
+			)}
 		</div>
 	);
 };
